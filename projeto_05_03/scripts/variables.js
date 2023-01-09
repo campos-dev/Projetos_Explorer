ee43cf0f2002ef0dc8567941bac30d2a1148a5f6
campos-dev <94444793+campos-dev@users.noreply.github.com>
2023-01-09 15:25:11 -0300

    Add files via upload

---
+++ projeto_05_03/scripts/variables.js
@@ -0,0 +1,8 @@
+const secondsDisplay = document.querySelector(".secondsDisplay");
+const minutesDisplay = document.querySelector(".minutesDisplay");
+const playBtn = document.querySelector(".playBtn");
+const setBtn = document.querySelector(".setBtn");
+const pauseBtn = document.querySelector(".pauseBtn");
+const stopBtn = document.querySelector(".stopBtn");
+
+export { secondsDisplay, minutesDisplay, playBtn, setBtn, pauseBtn, stopBtn };
