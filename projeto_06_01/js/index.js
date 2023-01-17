9ac48861ef7c906c0ab91d5d0807234774f29dbe
campos-dev <94444793+campos-dev@users.noreply.github.com>
2023-01-17 18:04:24 -0300

    Add files via upload

--- projeto_06_01/js/index.js
+++ projeto_06_01/js/index.js
@@ -2,9 +2,9 @@ import { Router } from "./router.js";
 
 const router = new Router();
 
-router.add("./", "./pages/home.html");
-router.add("./universe", "./pages/universe.html");
-router.add("./exploration", "./pages/exploration.html");
+router.add("/", "./pages/home.html");
+router.add("/universe", "./pages/universe.html");
+router.add("/exploration", "./pages/exploration.html");
 router.add(404, "./pages/404.html");
 
 window.routing = () => router.routing();
