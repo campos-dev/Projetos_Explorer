44767be30d43f2829a22a59293b17576eec1ef45
campos-dev <94444793+campos-dev@users.noreply.github.com>
2023-01-17 18:02:09 -0300

    Add files via upload

--- projeto_06_01/js/router.js
+++ projeto_06_01/js/router.js
@@ -26,13 +26,13 @@ export class Router {
   changeBgImage(pathname) {
     const img = document.querySelector("body img");
     switch (pathname) {
-      case "/":
+      case "./":
         img.src = "./img/mountains-universe-1 1.jpg";
         break;
-      case "/universe":
+      case "./universe":
         img.src = "./img/mountains-universe-2.jpg";
         break;
-      case "/exploration":
+      case "./exploration":
         img.src = "./img/mountains-universe-3.jpg";
         break;
       default:
